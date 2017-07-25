d11d2f6f740d5d8ee03666f2e37529cfc5609745
Softmosa <steven@softmosa.com>
2017-07-25 15:06:33 +0800

    git test

---
+++ J001/src/J001.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author ganrqunx
+ *
+ */
+public class J001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ J002/src/J002.java
@@ -0,0 +1,25 @@
+/**
+ * 實作時間記錄：
+ * 日期：<mm/dd>: <00:00>~<00:00>: <00:00>
+ * 程式目標：
+ * 設計規格：
+ * 設計想法：
+ * 虛擬碼：
+ *  
+ */
+
+/**
+ * @author ganrqunx
+ *
+ */
+public class J002 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, git again!!");
+
+	}
+
+}
